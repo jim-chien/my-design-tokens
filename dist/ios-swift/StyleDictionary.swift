977d6d9f1760b09afa0955a11d9f28fd4f7057a7
Jim Chien <jim_chien_br@hotmail.com>
2020-04-29 10:56:25 -0300

    test push build files

---
+++ dist/ios-swift/StyleDictionary.swift
@@ -0,0 +1,27 @@
+
+//
+// StyleDictionary.swift
+//
+// Do not edit directly
+// Generated on Wed, 29 Apr 2020 14:04:25 GMT
+//
+
+
+import UIKit
+
+public class StyleDictionary {
+    public static let colorSolidDark = UIColor(red: 0.298, green: 0.294, blue: 0.369, alpha:1)
+    public static let colorSolidError = UIColor(red: 0.851, green: 0.341, blue: 0.341, alpha:1)
+    public static let colorSolidExtraLight = UIColor(red: 0.949, green: 0.949, blue: 0.976, alpha:1)
+    public static let colorSolidLight = UIColor(red: 0.780, green: 0.780, blue: 0.831, alpha:1)
+    public static let colorSolidMedium = UIColor(red: 0.624, green: 0.624, blue: 0.686, alpha:1)
+    public static let colorSolidPrimary = UIColor(red: 0.000, green: 0.639, blue: 0.878, alpha:1)
+    public static let colorSolidSecondary = UIColor(red: 0.200, green: 0.247, blue: 0.282, alpha:1)
+    public static let colorSolidSuccess = UIColor(red: 0.502, green: 0.863, blue: 0.561, alpha:1)
+    public static let colorSolidWarning = UIColor(red: 1.000, green: 0.820, blue: 0.439, alpha:1)
+    public static let colorSolidWhite = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha:1)
+    public static let sizeFontBase = CGFloat(16.00)
+    public static let sizeFontLarge = CGFloat(32.00)
+    public static let sizeFontMedium = CGFloat(16.00)
+    public static let sizeFontSmall = CGFloat(12.00)
+}
